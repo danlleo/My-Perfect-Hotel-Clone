396e0b5abd98a60d6de2605279a3cdeed5b77486
Daniel Andrews <daniel.korienko@gmail.com>
2023-09-11 18:05:41 +0300

    Added WorldSpaceHoldUI class
    This class is responsible for keeping UI object in screen position converted from world position

---
+++ My Perfect Hotel Game/Assets/_Scripts/UI/Helpers/WorldSpaceHoldUI.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace UI.Helpers
+{
+    public class WorldSpaceHoldUI : MonoBehaviour
+    {
+        private Vector3 _stayAtScreenPosition;
+        private Vector3 _worldPosition;
+        private Action _onComplete;
+        private Camera _camera;
+        
+        private float _displayTime;
+        private float _timer;
+
+        private void Awake()
+            => _camera = Camera.main;
+
+        public void Initialize(Vector3 worldPosition, float displayTime, Action onComplete)
+        {
+            _worldPosition = worldPosition;
+            _displayTime = displayTime;
+            _onComplete = onComplete;
+        }
+
+        private void Update()
+        {
+            DisplayAtScreenPosition();
+        }
+
+        /// <summary>
+        /// This method will keep UI object in the screen position converted from worldPosition.
+        /// If camera is moving, UI object will still be displayed correctly.
+        /// </summary>
+        private void DisplayAtScreenPosition()
+        {
+            transform.position = _stayAtScreenPosition;
+            _stayAtScreenPosition = _camera.WorldToScreenPoint(_worldPosition);
+            _timer += Time.deltaTime;
+
+            if (_timer <= _displayTime)
+                _onComplete?.Invoke();
+        }
+    }
+}
