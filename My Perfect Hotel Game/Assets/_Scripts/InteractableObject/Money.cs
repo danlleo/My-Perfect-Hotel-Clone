fef4893e6a57fd014eb065a91419bf363d2c8226
Daniel Andrews <daniel.korienko@gmail.com>
2023-09-11 15:33:06 +0300

    Refactoring

--- My Perfect Hotel Game/Assets/_Scripts/InteractableObject/Money.cs
+++ My Perfect Hotel Game/Assets/_Scripts/InteractableObject/Money.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 namespace InteractableObject
 {
     [SelectionBase]
-    [DisallowMultipleComponent]
     public class Money : Interactable
     {
         public override void Interact()
